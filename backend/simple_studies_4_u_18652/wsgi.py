0565dde273578c462a1459798690f1663a43a029
Crowdbotics <team@crowdbotics.com>
2020-07-06 17:40:12 +0000

    Update models

--- backend/simple_studies_4_u_18652/wsgi.py
+++ backend/simple_studies_4_u_18652/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_studies_4_u_18652.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simple_studies_4_u_18652.settings")
 
 application = get_wsgi_application()
